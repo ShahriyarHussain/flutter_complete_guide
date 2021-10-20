5c5ced3c65fb647efb0972ada8153f2fd9629f07
ShahriyarHussain <shossainshihab36@gmail.com>
2021-10-21 01:00:44 +0600

    Remove boilerplates

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,7 @@ void main() {
 class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Text('Welcome!'),
+      home: Text('more!'),
     );
   }
 }
