08bdc4d60871d5cd2131ca1c312c54dbebb73768
ShahriyarHussain <shossainshihab36@gmail.com>
2021-10-22 00:35:17 +0600

    Add button interactions

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 // void main() {
@@ -6,17 +8,55 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return MyAppState();
+  }
+}
+
+class MyAppState extends State<MyApp> {
+  var questionIndex = 0;
+
+  void answerQuestion() {
+    setState(() {
+      questionIndex = Random().nextInt(5);
+    });
+    print("Answer Chosen");
+  }
+
   @override
   Widget build(BuildContext context) {
-    // String text = "";
+    var questions = [
+      "What's you school?",
+      "What's your height?",
+      "What's your name?",
+      "What's your dogs name?",
+      "What's this app doing ?"
+    ];
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text('First App'),
         ),
         backgroundColor: (Colors.white),
-        body: Text('First App Body'),
+        body: Column(
+          children: [
+            Text(questions.elementAt(questionIndex)),
+            RaisedButton(
+              child: Text('Answer 1'),
+              onPressed: answerQuestion,
+            ),
+            RaisedButton(
+              child: Text('Answer 2'),
+              onPressed: () => print("Answer 3"),
+            ),
+            RaisedButton(
+              child: Text('Answer 3'),
+              onPressed: () => print("Answer 2"),
+            )
+          ],
+        ),
       ),
     );
   }
