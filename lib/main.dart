9845af9228da1706c8d9b951f0c049447af0ce5e
ShahriyarHussain <shossainshihab36@gmail.com>
2021-10-21 13:49:59 +0600

    Add new appbar and bg

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,23 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
-}
+// void main() {
+//   runApp(MyApp());
+// }
+
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  @override
   Widget build(BuildContext context) {
+    // String text = "";
     return MaterialApp(
-      home: Text('more!'),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('First App'),
+        ),
+        backgroundColor: (Colors.white),
+        body: Text('First App Body'),
+      ),
     );
   }
 }
